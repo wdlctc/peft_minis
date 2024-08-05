82a8afc456c14c941c518ca9db7069b05673f709
Your Name <you@example.com>
2024-08-05 15:03:49 +0000

    update

--- examples/sft/train.py
+++ examples/sft/train.py
@@ -121,7 +121,7 @@ def main(model_args, data_args, training_args):
         apply_chat_template=model_args.chat_template_format != "none",
     )
 
-    # model = minisequence(model)
+    model = minisequence(model)
 
     # trainer
     trainer = SFTTrainer(
